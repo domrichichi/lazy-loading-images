55d8917c37c93576aa8c160c6d21f08d65b86edf
domrichichi <domrichichi@Doms-MacBook-Pro.local>
2016-11-28 16:38:52 -0500

    COMMIT

--- js/image-loader.js
+++ js/image-loader.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', function (e)) {
+window.addEventListener('load', function (e) {
 
 	var imgs = document.querySelectorAll('.non-critical-img');
 
-	[].forEach.call(imgs, function(img) {
-		var imgTag = img.queSelector('img');
+	[].forEach.call(imgs, function (img) {
+		var imgTag = img.querySelector('img');
 
 		imgTag.src = imgTag.dataset.src;
 		imgTag.removeAttribute('hidden');
